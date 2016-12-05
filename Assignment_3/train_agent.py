82b705b5a87982c3efb3407c2ed0a98541ce3e33
Mohamed Abou-Hussein <abouhussein.mohamed@gmail.com>
2016-12-05 01:05:12 +0100

    trials

--- Assignment_3/train_agent.py
+++ Assignment_3/train_agent.py
@@ -5,6 +5,13 @@ import tensorflow as tf
 from utils     import Options
 from simulator import Simulator
 from transitionTable import TransitionTable
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Convolution2D, MaxPooling2D, Dropout, Flatten
+
+class cnn_model:
+	def __init__(self):
+		self.model = Sequential()
+
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # NOTE:
@@ -19,6 +26,7 @@ sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
 trans = TransitionTable(opt.state_siz, opt.act_num, opt.hist_len,
                              opt.minibatch_size, opt.valid_size,
                              opt.states_fil, opt.labels_fil)
+cnn_m = cnn_model()
 
 # 1. train
 ######################################
@@ -34,6 +42,64 @@ trans = TransitionTable(opt.state_siz, opt.act_num, opt.hist_len,
 # for i in range(number_of_batches):
 #     x, y = trans.sample_minibatch()
 ######################################
+
+# Get train and validation data
+train_data = trans.get_train()
+valid_data = trans.get_valid()
+
+
+
+# Initialize keras model
+# model = Sequential()
+
+# Create cnn
+print(train_data[0].shape)
+cnn_m.model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=train_data[0].shape))
+cnn_m.model.add(Activation('relu'))
+cnn_m.model.add(Convolution2D(32, 1, 3))
+cnn_m.model.add(Activation('relu'))
+cnn_m.model.add(MaxPooling2D(pool_size=(1, 2)))
+cnn_m.model.add(Dropout(0.25))
+
+cnn_m.model.add(Convolution2D(64, 1, 3, border_mode='same'))
+cnn_m.model.add(Activation('relu'))
+cnn_m.model.add(Convolution2D(64, 1, 3))
+cnn_m.model.add(Activation('relu'))
+cnn_m.model.add(MaxPooling2D(pool_size=(1, 2)))
+cnn_m.model.add(Dropout(0.25))
+
+cnn_m.model.add(Flatten())
+cnn_m.model.add(Dense(512))
+cnn_m.model.add(Activation('relu'))
+cnn_m.model.add(Dropout(0.5))
+cnn_m.model.add(Dense(5))
+cnn_m.model.add(Activation('softmax'))
+
+
+"""
+self.model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape = train_data[0].shape))
+print("print train_data[0].shape ", train_data[0].shape)
+#self.model.add(Dense(output_dim=64, input_dim=train_data.shape))
+self.model.add(Activation("relu"))
+self.model.add(Activation("softmax"))
+"""
+
+# Define attributes of the cnn; categorial, optimizer_type, performance metrics
+cnn_m.model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# Define values for SGD optimizer
+from keras.optimizers import SGD
+cnn_m.model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
+
+# Fit the model to the training data
+cnn_m.model.fit(train_data[0], train_data[1], nb_epoch=20, batch_size=32)
+
+# Compute loss metrics for the current model i.e, training error
+cnn_m.model.loss_and_metrics = cnn_m.model.evaluate(valid_data[0], valid_data[1], batch_size=32)
+print("Valid error: ", cnn_m.model.loss_and_metrics)
+
+
+
 
 # 2. save your trained model
 
